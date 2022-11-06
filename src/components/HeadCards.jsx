3eff28a9db06dd9b7db9aabcb9a4eadab46b5979
Jirafek <nikolaevserafim15@gmail.com>
2022-11-06 14:12:47 +0300

    feat: images on cards + animation fix

--- src/components/HeadCards.jsx
+++ src/components/HeadCards.jsx
@@ -14,7 +14,7 @@ const HeadCards = () => {
                     className='w-[17.5rem] h-10 bg-[#E6FFCD] rounded-[5px] flex justify-center items-center absolute top-0 left-1/2 z-10'>
                         {el.name}
                     </div>
-                    <div className='h-[11.25rem] w-full bg-[#F3F7FF] rounded-[5px] align-bottom pt-[3.125rem] pl-[1.875rem]'>
+                    <div className='h-[11.25rem] w-full bg-[#F3F7FF] rounded-[5px] align-bottom pt-[3.125rem] pl-[1.875rem] relative'>
                         <ul className='text-[0.6875rem] flex flex-col text-[#3C517B] mb-10'>
                             {el.items.map(itm_el => {
                                 return (
@@ -22,14 +22,15 @@ const HeadCards = () => {
                                 );
                             })}
                         </ul>
-                        <button className='bg-white text-[0.6875rem] font-bold flex h-[1.875rem] group'>
+                        <button className='bg-white text-[0.6875rem] font-bold flex h-[1.875rem] head-group_btn'>
                             <div className='flex items-center justify-center rounded-l-[5px] w-[8.75rem] h-full bg-[#2B73FF] text-white'>
                                 Перейти
                             </div>
                             <div className='w-10 h-full flex items-center justify-center border border-[#2B73FF] rounded-r-[3px] '>
-                                <img className='w-[15px] duration-500 bg-white group-hover:translate-x-[5px]' src="/img/arrow.png" alt="" />
+                                <img className='w-[15px] duration-500 bg-white head-arrow' src="/img/arrow.png" alt="" />
                             </div>
                         </button>
+                        <img className='absolute right-0 bottom-0' style={{ width: el.image.width, height: el.image.height }} src={el.image.src} alt="" />
                     </div>
                 </div>
             );
